531810d0841af9537559f67be87b8e8a10e31250
April Rauenhorst <april@southern-energy.com>
2023-01-04 14:43:42 -0500

    1-3-23

--- duplicate_printer.py
+++ duplicate_printer.py
@@ -18,11 +18,11 @@ import numpy as np
 
 # The current_sheet variable needs to be named the sheet you want to check for Duplicate (Service Types &Addresses) OR duplicate (ServiceIDs).
 
-current_sheet = 'C:/Users/april/Documents/Invoicing/12-30-22.xlsx'
+current_sheet = 'C:/Users/april/Documents/Invoicing/1-3-23.xlsx'
 
-large_sheet = 'S:/SEM/Building Science Team/Accounting/Invoicing Spreadsheets/2022 Inspections Billing.xlsx'
+large_sheet = 'S:/SEM/Building Science Team/Accounting/Invoicing Spreadsheets/2023 Inspections Billing.xlsx'
 
-large_sheet_2 = 'S:/SEM/Building Science Team/Accounting/Invoicing Spreadsheets/2021 Inspections Billing.xlsx'
+large_sheet_2 = 'S:/SEM/Building Science Team/Accounting/Invoicing Spreadsheets/2022 Inspections Billing.xlsx'
 
 # =======================================================
 # =======================================================
